a02fb8778563e22c0c3cfc4f129a18d7d0b72a84
scfreiburg09 <simonschneider02@t-online.de>
2022-10-29 21:00:19 +0200

    Added basic time-handling functions

--- Terminplaner/Utils/Calendar.js
+++ Terminplaner/Utils/Calendar.js
@@ -1,3 +1,42 @@
+//Selects all events from calendar that start at of after rangeStart and before rangeEnd
 export function getEventsWithinRange(calendar, rangeStart, rangeEnd){
-    return []
+    function isInRange(event){
+        const eventStart = new Date(event.start);
+        if ((eventStart >= rangeStart) && (rangeEnd > eventStart)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return calendar.filter(isInRange);
 }
+
+//Returns the date object for today, 0:00
+export function getTodayTimestamp(){
+    const now = new Date();
+    const dateOfMonth = now.getDate();
+    const month = now.getMonth();
+    const year = now.getFullYear();
+
+    const startOfToday = new Date(year, month, dateOfMonth);
+    return startOfToday
+}
+
+//Returns the date object for tomorrow, 0:00
+export function getStartTomorrowTimestamp(){
+    const today = getTodayTimestamp();
+    const oneDayMilliseconds = 24*60*60*1000;
+
+    const startOfTomorrow = new Date(today.getTime() + oneDayMilliseconds);
+    return startOfTomorrow;
+}
+
+//Returns the date object for tomorrow, 24:00
+export function getEndTomorrowTimestamp(){
+    const today = getTodayTimestamp();
+    const oneDayMilliseconds = 24*60*60*1000;
+
+    const endOfTomorrow = newDate(today.getTime() + (2 * oneDayMilliseconds))
+    return endOfTomorrow;
+}
+
