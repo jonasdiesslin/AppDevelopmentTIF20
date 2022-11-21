85c8197c81d7cd0528241d0ceafc7361e238cf18
scfreiburg09 <simonschneider02@t-online.de>
2022-11-21 18:13:14 +0100

    Started Firebase Integration

---
+++ Terminplaner/Utils/fbStorage.js
@@ -0,0 +1,29 @@
+import { initializeApp } from 'firebase/app';
+
+// Optionally import the services that you want to use
+// import {...} from "firebase/auth";
+// import {...} from "firebase/database";
+// import {...} from "firebase/firestore";
+// import {...} from "firebase/functions";
+// import {...} from "firebase/storage";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDtoXY_PF4N9T_gjaahc_WizlmGtdbHacA",
+  
+    authDomain: "terminplaner-f5a14.firebaseapp.com",
+  
+    projectId: "terminplaner-f5a14",
+  
+    storageBucket: "terminplaner-f5a14.appspot.com",
+  
+    messagingSenderId: "404188597929",
+  
+    appId: "1:404188597929:web:66b87373d424ec6deaa3b8"
+  
+};
+  
+
+const app = initializeApp(firebaseConfig);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
